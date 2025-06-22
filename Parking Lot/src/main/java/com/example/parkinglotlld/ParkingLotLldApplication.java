6722d112ab578b087434ae2ae24c53352e2a15ab
Koushal Jha <koushaljha887@gmail.com>
2025-06-22 18:48:30 +0530

    parking lot commit

---
+++ Parking Lot/src/main/java/com/example/parkinglotlld/ParkingLotLldApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkinglotlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotLldApplication.class, args);
+    }
+
+}
---
+++ Parking Lot/src/test/java/com/example/parkinglotlld/ParkingLotLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkinglotlld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotLldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
